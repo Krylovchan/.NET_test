7f4ba0846a92f208a391b76fe0ee8359a587ad48
Krylovchan <egor.krylov02@mail.ru>
2023-09-06 00:04:55 +0700

    f

--- TodoApi/Services/ITodoRepository.cs
+++ TodoApi/Services/ITodoRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace TodoApi.Services
+﻿using TodoApi.Models;
+
+namespace TodoApi.Services
 {
     public interface ITodoRepository
     {
+        void Get(long id);
+        void GetAll();
+        void Add(TodoItemDTO todoDTO);
+        void Remove(long id);
+        void Edit(long id, TodoItemDTO todoDTO);
+
     }
 }
---
+++ TodoApi/Services/TodoRepository.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace TodoApi.Services
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoRepository : ITodoRepository
+    {
+        private readonly TodoContext _context;
+
+        public TodoRepository(TodoContext context)
+        {
+            _context = context;
+        }
+
+        public void Get(long id)
+        { 
+        
+        }
+        [HttpGet]
+        public void GetAll()
+        {
+            return await _context.TodoItems
+                .Select(x => ItemToDTO(x))
+                .ToListAsync();
+        }
+        public void Add(TodoItemDTO todoDTO)
+        {
+
+        }
+        public void Remove(long id)
+        {
+
+        }
+        
+        public void Edit(long id, TodoItemDTO todoDTO)
+        {
+            if (id != todoDTO.Id)
+            {
+                return BadRequest();
+            }
+
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            todoItem.Name = todoDTO.Name;
+            todoItem.IsComplete = todoDTO.IsComplete;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException) when (!TodoItemExists(id))
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+    }
+}
